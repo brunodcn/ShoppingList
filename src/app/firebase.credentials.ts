f9c079c603b4816f42d6e6b85286c278434dfb65
Bruno Negrões <brunodcn45@gmail.com>
2017-08-28 11:15:58 -0300

    first commit

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,9 @@
+export const FIREBASE_CREDENTIALS = {
+	
+    apiKey: "AIzaSyCzHRb-RKQJqDBif5qozUqsBBtZA9Ji5jw",
+    authDomain: "fir-crud-f6e14.firebaseapp.com",
+    databaseURL: "https://fir-crud-f6e14.firebaseio.com",
+    projectId: "fir-crud-f6e14",
+    storageBucket: "fir-crud-f6e14.appspot.com",
+    messagingSenderId: "824307157870"
+  };
---
+++ src/pages/edit-shopping-item/edit-shopping-item.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database';
+import { Subscription } from 'rxjs/Subscription';
+
+import { ShoppingItem } from '../../models/shopping-item/shopping-item.interface';
+
+@Component({
+  selector: 'page-edit-shopping-item',
+  templateUrl: 'edit-shopping-item.html',
+})
+export class EditShoppingItemPage {
+
+	shoppingItemSubscription: Subscription;
+	shoppingItemRef$: FirebaseObjectObservable<ShoppingItem>;
+	shoppingItem = {} as ShoppingItem;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private database: AngularFireDatabase) {
+
+  const shoppingItemId = this.navParams.get('shoppingItemId');
+  this.shoppingItemRef$ = this.database.object(`shopping-list/${shoppingItemId}`);
+
+  this.shoppingItemSubscription = this.shoppingItemRef$.subscribe(shoppingItem => this.shoppingItem = shoppingItem);
+
+  }
+
+  editShoppingItem(shoppingItem: ShoppingItem){
+  this.shoppingItemRef$.update(shoppingItem);
+
+  this.navCtrl.pop();
+
+  }
+
+ionViewWillLeave(){
+	this.shoppingItemSubscription.unsubscribe();
+}
+
+}
